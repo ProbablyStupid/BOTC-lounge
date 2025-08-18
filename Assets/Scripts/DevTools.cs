f458a020629a1b7404d49f29cb5b9e1dc0e204ce
ProbablyStupid <thomas.kaliwe@gmail.com>
2025-08-18 13:00:28 +0200

    FPS multiplayer game initial

---
+++ Assets/Scripts/DevTools.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DevTools : MonoBehaviour
+{
+    public void Update()
+    {
+        // TODO: implement visual overlay key-switch here
+        // 
+
+        //Debug.Log("Update is running!");
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // There should really be assert-conditions for these components being undefined.
+    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] float speed = 1.0f;
+    [Header("This is useless for now. Gravity is handled by the global physics engine. Maybe a TODO?")]
+    [SerializeField] float gravity = 9.8f;
+
+    [SerializeField] Camera playerCamera;
+    [SerializeField] float xSensitivity = 1.0f, ySensitivity = 1.0f;
+    [SerializeField] float mouseClampMin = -90.0f;
+    [SerializeField] float mouseClampMax = 90.0f;
+
+    [SerializeField] private float xRotation = 0.0f;
+
+    void Update()
+    {
+        Vector3 movementDirection = transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal");
+        rigidbody.AddForce(movementDirection * speed * Time.deltaTime);
+        Debug.Log("Added force of: " +  movementDirection * speed * Time.deltaTime);
+
+        // --- mouse movement ---
+
+        float yRotation = Input.GetAxisRaw("Mouse X") * ySensitivity;
+        xRotation -= Input.GetAxisRaw("Mouse Y") * xSensitivity;
+        xRotation = Mathf.Clamp(xRotation, mouseClampMin, mouseClampMax);
+
+        // rotates the player
+        transform.Rotate(Vector3.up, yRotation);
+        
+        // rotates the camera
+        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+    }
+}
